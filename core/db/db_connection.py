d7940019942516f5ae5230d43d58616accf5712f
Hamoud9876 <h.ghaleb95@gmail.com>
2025-06-16 14:07:49 +0100

    update: building the connection to the database

---
+++ core/db/db_connection.py
@@ -0,0 +1,19 @@
+from pg8000.native import Connection
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+
+
+def db_connection():
+    return Connection(
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_DATABASE"),
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT")
+    )
+
+
+def close_db(conn):
+    conn.close()
