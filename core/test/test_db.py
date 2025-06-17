747010f68f9c0e443d4a44641758be030f073ce8
Hamoud9876 <h.ghaleb95@gmail.com>
2025-06-17 11:04:07 +0100

    feat: adding the test for db_connection function

---
+++ core/test/test_db.py
@@ -0,0 +1,33 @@
+from unittest.mock import patch, MagicMock
+from db.db_connection import db_connection,close_db
+import os
+
+class TestDBConnection():
+    @patch.dict(os.environ, {
+    "DB_USER": "test_user",
+    "DB_PASSWORD": "test_pass",
+    "DB_DATABASE": "test_db",
+    "DB_HOST": "localhost",
+    "DB_PORT": "5432"
+})
+    @patch("db.db_connection.Connection")
+    def test_db_connection(self,mock_connection):
+        mock_conn_instance = MagicMock()
+        mock_connection.return_value = mock_conn_instance
+
+        conn =db_connection()
+
+        mock_connection.assert_called_once_with(
+            user="test_user",
+            password="test_pass",
+            database="test_db",
+            host="localhost",
+            port="5432"
+        )
+        assert conn == mock_conn_instance
+
+
+    def test_close_db(self):
+        mock_conn = MagicMock()
+        close_db(mock_conn)
+        mock_conn.close.assert_called_once()
