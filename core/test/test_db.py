8d3d3bcf28e788659b169f255daf29913f282c05
Hamoud9876 <h.ghaleb95@gmail.com>
2025-06-25 19:56:31 +0100

    update: changed the imports and the patch

--- core/test/test_db.py
+++ core/test/test_db.py
@@ -1,31 +1,34 @@
 from unittest.mock import patch, MagicMock
-from db.db_connection import db_connection,close_db
+from core.database.db_connection import db_connection, close_db
 import os
 
-class TestDBConnection():
-    @patch.dict(os.environ, {
-    "DB_USER": "test_user",
-    "DB_PASSWORD": "test_pass",
-    "DB_DATABASE": "test_db",
-    "DB_HOST": "localhost",
-    "DB_PORT": "5432"
-})
-    @patch("db.db_connection.Connection")
-    def test_db_connection(self,mock_connection):
+
+class TestDBConnection:
+    @patch.dict(
+        os.environ,
+        {
+            "DB_USER": "test_user",
+            "DB_PASSWORD": "test_pass",
+            "DB_DATABASE": "test_db",
+            "DB_HOST": "localhost",
+            "DB_PORT": "5432",
+        },
+    )
+    @patch("core.database.db_connection.Connection")
+    def test_db_connection(self, mock_connection):
         mock_conn_instance = MagicMock()
         mock_connection.return_value = mock_conn_instance
 
-        conn =db_connection()
+        conn = db_connection()
 
         mock_connection.assert_called_once_with(
             user="test_user",
             password="test_pass",
             database="test_db",
             host="localhost",
-            port="5432"
+            port="5432",
         )
         assert conn == mock_conn_instance
-
 
     def test_close_db(self):
         mock_conn = MagicMock()
