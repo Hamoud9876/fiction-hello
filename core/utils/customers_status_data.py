953e2aaa0d88d6253ccf73af8604c32253b121cc
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-01 21:54:28 +0100

    update: added the new history functions invokation to complete customer data

--- core/utils/customers_status_data.py
+++ core/utils/customers_status_data.py
@@ -2,6 +2,7 @@ from core.data.data import customers_status
 from random import randint
 from core.utils.cust_hist_active import cust_hist_active
 from core.utils.cust_hist_inactive import cust_hist_inactive
+from core.utils.cust_hist_long_changes import cust_hist_long_changes
 from datetime import date, datetime
 from faker import Faker
 
@@ -12,22 +13,14 @@ def customers_status_data(join_date: date, cust_id:int):
     rand_num = randint(0,9)
     
     #customer active since joining
-    if rand_num >2:
-        cust_hist_active(customers_status, join_date,cust_id)
+    if rand_num >3:
+        return cust_hist_active(customers_status, join_date,cust_id)
 
     #customer not active anynore
-    elif rand_num ==2:
-        cust_hist_inactive(customers_status, join_date,cust_id)
+    elif rand_num ==3:
+        return cust_hist_inactive(customers_status, join_date,cust_id)
         
     #complecated history of multiple changes
-    elif rand_num > 7:
-        pass
-    #complecated history of departure or blocked
     else:
-        pass
-
-
-    rand_num = randint(0,3)
-    customer_status = customers_status[rand_num]
-    return {"customer_status": customer_status,
-            }
+        return cust_hist_long_changes(customers_status, join_date,cust_id)
+    
