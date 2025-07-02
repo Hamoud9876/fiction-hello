1979874eddc6ce0e08a387196b99dde10dda22fd
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-02 20:12:42 +0100

    feat: new function that creates a random post code

---
+++ core/utils/generate_post_code.py
@@ -0,0 +1,21 @@
+from random import randint
+import string
+import secrets
+
+
+def generate_post_code():
+    """
+    generate a random post code
+    -----------------------------------------
+    args:  None
+    -----------------------------------------
+    return: string representing a post code
+    """
+     
+    alphabet = string.ascii_letters 
+    post_code = (''.join(secrets.choice(alphabet) 
+                         if i in [0,4,3] else
+                         str(randint(1,20)) for i in range(4)))
+    post_code = post_code.upper()
+
+    return post_code
