780a253677ad81b80a4a797cb19a2bb53412fe12
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-02 20:02:26 +0100

    update: added validation for the recieved input

--- core/utils/days_between.py
+++ core/utils/days_between.py
@@ -2,4 +2,8 @@ from datetime import date
 
 
 def days_between(d1: date, d2: date):
+        if not isinstance(d1, date):
+                return "First date is not valid"
+        if not isinstance(d2, date):
+                return "Second date is not valid"
         return abs((d2-d1).days)
