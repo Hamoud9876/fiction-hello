1d8d4a57cbf0d1c6a8679fc61fdd4b3bd8ae0e1e
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-02 20:04:34 +0100

    update: added doc string

--- core/utils/days_between.py
+++ core/utils/days_between.py
@@ -2,6 +2,14 @@ from datetime import date
 
 
 def days_between(d1: date, d2: date):
+        """
+        calculate the days diff between 2 dates
+        -----------------------------------------
+        args:  d1: first date
+        d1: second date
+        -----------------------------------------
+        return: an int representing the difference in days
+        """
         if not isinstance(d1, date):
                 return "First date is not valid"
         if not isinstance(d2, date):
