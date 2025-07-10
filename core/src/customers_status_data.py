2c1b9f9f43611a2e1d4b776d8b35b1382889fb55
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-10 22:41:31 +0100

    update: removed cust_id as it is not used

--- core/src/customers_status_data.py
+++ core/src/customers_status_data.py
@@ -6,7 +6,7 @@ from core.src.cust_hist_long_changes import cust_hist_long_changes
 from datetime import date
 
 
-def customers_status_data(join_date: date, cust_id: int):
+def customers_status_data(join_date: date):
     """
     creates customer status history and current status
     -----------------------------------------
@@ -18,18 +18,18 @@ def customers_status_data(join_date: date, cust_id: int):
     along with the status history if exists
     """
 
-    if not isinstance(join_date, date) or not isinstance(cust_id, int):
+    if not isinstance(join_date, date):
         return "Not a valid input"
     rand_num = randint(0, 9)
 
     # customer active since joining
     if rand_num > 3:
-        return cust_hist_active(customers_status, join_date, cust_id)
+        return cust_hist_active(customers_status, join_date)
 
     # customer not active anynore
     elif rand_num == 3:
-        return cust_hist_inactive(customers_status, join_date, cust_id)
+        return cust_hist_inactive(customers_status, join_date)
 
     # complecated history of multiple changes
     else:
-        return cust_hist_long_changes(customers_status, join_date, cust_id)
+        return cust_hist_long_changes(customers_status, join_date)
