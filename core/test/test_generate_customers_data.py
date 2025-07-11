57ac8903141612cc85ce2369ccb764160970067d
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-11 19:44:53 +0100

    update: changed some test and added new ones

--- core/test/test_generate_customers_data.py
+++ core/test/test_generate_customers_data.py
@@ -9,7 +9,6 @@ from core.utils.generate_post_code import generate_post_code
 from core.utils.days_between import days_between
 from core.utils.random_string import random_string
 from unittest.mock import patch
-import pytest
 from datetime import datetime, date
 
 
@@ -23,44 +22,38 @@ class TestGenerateCustomerData:
 
     def test_output_seccessful(self):
         result = generate_customers_data(1)
-        assert isinstance(result, dict)
-        assert isinstance(result["customers"], list)
+        assert isinstance(result, list)
+        assert isinstance(result[0], dict)
 
-    def test_create_1_customer(self):
+    def test_create_one_customer(self):
         result = generate_customers_data(1)
-        assert len(result["customers"]) == 1
+        assert len(result) == 1
 
-    @pytest.mark.skip
     def test_create_multiple_customers(self):
         result = generate_customers_data(2)
-        assert len(result["customers"]) == 2
+        assert len(result) == 2
 
         result = generate_customers_data(13)
-        assert len(result["customers"]) == 13
+        assert len(result) == 13
 
         result = generate_customers_data(50)
-        assert len(result["customers"]) == 50
+        assert len(result) == 50
 
         result = generate_customers_data(600)
-        assert len(result["customers"]) == 600
+        assert len(result) == 600
 
 
 class TestCustomerStatus:
     def test_handle_wrong_input(self):
-        date = "10/11/2025"
-        result = customers_status_data(join_date=date, cust_id=1)
-        assert result == "Not a valid input"
-
-        date_obj = datetime.strptime(date, "%d/%m/%Y")
-        date_obj = date_obj.date()
-        result = customers_status_data(join_date=date_obj, cust_id="hi")
+        date = "10/1/2025"
+        result = customers_status_data(join_date=date)
         assert result == "Not a valid input"
 
     def test_return_dict(self):
         date = "10/2/2025"
         date_obj = datetime.strptime(date, "%d/%m/%Y")
         date_obj = date_obj.date()
-        result = customers_status_data(join_date=date_obj, cust_id=1)
+        result = customers_status_data(join_date=date_obj)
 
         assert isinstance(result, dict)
 
@@ -68,45 +61,71 @@ class TestCustomerStatus:
         date = "10/2/2025"
         date_obj = datetime.strptime(date, "%d/%m/%Y")
         date_obj = date_obj.date()
-        result = customers_status_data(join_date=date_obj, cust_id=1)
+        result = customers_status_data(join_date=date_obj)
 
         assert "customer_status" in result
         assert isinstance(result["customer_status"], str)
+
+    @patch("core.src.customers_status_data.randint")
+    def test_returns_hist_active(self, mock_randint):
+        mock_randint.return_value = 2
+
+        date = "10/2/2025"
+        date_obj = datetime.strptime(date, "%d/%m/%Y")
+        date_obj = date_obj.date()
+        result = customers_status_data(join_date=date_obj)
+
+        assert result["customer_status"] == "Active"
+
+    @patch("core.src.customers_status_data.randint")
+    def test_returns_hist_inactive(self, mock_randint):
+        mock_randint.return_value = 3
+
+        date = "10/2/2025"
+        date_obj = datetime.strptime(date, "%d/%m/%Y")
+        date_obj = date_obj.date()
+        result = customers_status_data(join_date=date_obj)
+
+        assert result["customer_status"] == "Inactive"
+
+    @patch("core.src.customers_status_data.randint")
+    def test_returns_hist_long(self, mock_randint):
+        mock_randint.return_value = 3
+
+        date = "10/2/2025"
+        date_obj = datetime.strptime(date, "%d/%m/%Y")
+        date_obj = date_obj.date()
+        result = customers_status_data(join_date=date_obj)
+
+        assert result["customer_status"] in customers_status
 
 
 class TestCustHistActive:
     def test_handles_wrong_input(self):
         date = "10/2/2025"
         date_obj = datetime.strptime(date, "%d/%m/%Y")
-        result = cust_hist_active({}, date_obj, 1)
+        result = cust_hist_active({}, date_obj)
         assert result == "Not a valid customer status input"
 
-        result = cust_hist_active(customers_status, date, 1)
-        assert result == "Not a valid date input"
-
-        result = cust_hist_active(customers_status, date_obj, "1")
-        assert result == "Not a valid customer id"
-
-        result = cust_hist_active(customers_status, date, "1")
+        result = cust_hist_active(customers_status, date)
         assert result == "Not a valid date input"
 
     def test_return_dict(self):
         date = "10/2/2025"
         date_obj = datetime.strptime(date, "%d/%m/%Y")
-        result = cust_hist_active(customers_status, date_obj, 1)
+        result = cust_hist_active(customers_status, date_obj)
 
         assert isinstance(result, dict)
 
     def test_return_expected_output(self):
         join_date = "10/2/2025"
         date_obj = datetime.strptime(join_date, "%d/%m/%Y")
-        result = cust_hist_active(customers_status, date_obj, 1)
+        result = cust_hist_active(customers_status, date_obj)
 
         assert "customer_status" in result
         assert "cust_sts_hist" in result
         assert isinstance(result["cust_sts_hist"], list)
         assert isinstance(result["cust_sts_hist"][0]["customer_status"], str)
-        assert isinstance(result["cust_sts_hist"][0]["customer_id"], int)
         assert isinstance(result["cust_sts_hist"][0]["change_date"], date)
 
 
@@ -114,35 +133,28 @@ class TestCustHistInactive:
     def test_handles_wrong_input(self):
         date = "10/2/2025"
         date_obj = datetime.strptime(date, "%d/%m/%Y")
-        result = cust_hist_inactive({}, date_obj, 1)
+        result = cust_hist_inactive({}, date_obj)
         assert result == "Not a valid customer status input"
 
-        result = cust_hist_inactive(customers_status, date, 1)
-        assert result == "Not a valid date input"
-
-        result = cust_hist_inactive(customers_status, date_obj, "1")
-        assert result == "Not a valid customer id"
-
-        result = cust_hist_inactive(customers_status, date, "1")
+        result = cust_hist_inactive(customers_status, date)
         assert result == "Not a valid date input"
 
     def test_return_dict(self):
         date = "10/2/2025"
         date_obj = datetime.strptime(date, "%d/%m/%Y")
-        result = cust_hist_inactive(customers_status, date_obj, 1)
+        result = cust_hist_inactive(customers_status, date_obj)
 
         assert isinstance(result, dict)
 
     def test_return_expected_output(self):
         join_date = "10/2/2025"
         date_obj = datetime.strptime(join_date, "%d/%m/%Y")
-        result = cust_hist_active(customers_status, date_obj, 1)
+        result = cust_hist_active(customers_status, date_obj)
 
         assert "customer_status" in result
         assert "cust_sts_hist" in result
         assert isinstance(result["cust_sts_hist"], list)
         assert isinstance(result["cust_sts_hist"][0]["customer_status"], str)
-        assert isinstance(result["cust_sts_hist"][0]["customer_id"], int)
         assert isinstance(result["cust_sts_hist"][0]["change_date"], date)
 
 
@@ -151,23 +163,17 @@ class TestCustHistLongChanges:
         date = "10/2/2025"
         date_obj = datetime.strptime(date, "%d/%m/%Y")
         date_obj = date_obj.date()
-        result = cust_hist_long_changes({}, date_obj, 1)
+        result = cust_hist_long_changes({}, date_obj)
         assert result == "Not a valid customer status input"
 
-        result = cust_hist_long_changes(customers_status, date, 1)
-        assert result == "Not a valid date input"
-
-        result = cust_hist_long_changes(customers_status, date_obj, "1")
-        assert result == "Not a valid customer id"
-
-        result = cust_hist_long_changes(customers_status, date, "1")
+        result = cust_hist_long_changes(customers_status, date)
         assert result == "Not a valid date input"
 
     def test_return_dict(self):
         date = "10/2/2025"
         date_obj = datetime.strptime(date, "%d/%m/%Y")
         date_obj = date_obj.date()
-        result = cust_hist_long_changes(customers_status, date_obj, 1)
+        result = cust_hist_long_changes(customers_status, date_obj)
 
         assert isinstance(result, dict)
 
@@ -175,40 +181,40 @@ class TestCustHistLongChanges:
         join_date = "10/2/2010"
         date_obj = datetime.strptime(join_date, "%d/%m/%Y")
         date_obj = date_obj.date()
-        result = cust_hist_long_changes(customers_status, date_obj, 1)
+        result = cust_hist_long_changes(customers_status, date_obj)
 
         assert "customer_status" in result
         assert "cust_sts_hist" in result
         assert isinstance(result["cust_sts_hist"], list)
         assert isinstance(result["cust_sts_hist"][0]["customer_status"], str)
-        assert isinstance(result["cust_sts_hist"][0]["customer_id"], int)
         assert isinstance(result["cust_sts_hist"][0]["change_date"], date)
 
 
 class TestAddressData:
     def test_return_dict(self):
         result = generate_address_data()
-        assert isinstance(result, dict)
+        assert isinstance(result, list)
 
     @patch("core.src.generate_address_data.randint")
     def test_generates_one_address(self, mock_rand_int):
         mock_rand_int.return_value = 2
         result = generate_address_data()
-        assert len(result["address"]) == 1
-        assert isinstance(result["address"][0]["fist_line"], str)
-        assert isinstance(result["address"][0]["second_line"], str)
-        assert isinstance(result["address"][0]["city"], str)
-        assert isinstance(result["address"][0]["county"], str)
-        assert isinstance(result["address"][0]["post_code"], str)
-        assert result["address"][0]["address_type"] == "Home"
-        assert isinstance(result["address"][0]["address_type"], str)
+        assert len(result) == 1
+        for i in result:
+            assert isinstance(i["fist_line"], str)
+            assert isinstance(i["second_line"], str)
+            assert isinstance(i["city"], str)
+            assert isinstance(i["county"], str)
+            assert isinstance(i["post_code"], str)
+            assert i["address_type"] in ["Home", "Billing"]
+            assert isinstance(i["address_type"], str)
 
     @patch("core.src.generate_address_data.randint")
     def test_generate_two_addresses(self, mock_rand_int):
         mock_rand_int.return_value = 1
         result = generate_address_data()
-        assert len(result["address"]) == 2
-        for i in result["address"]:
+        assert len(result) == 2
+        for i in result:
             assert isinstance(i["fist_line"], str)
             assert isinstance(i["second_line"], str)
             assert isinstance(i["city"], str)
