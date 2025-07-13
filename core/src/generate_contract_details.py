4c6ead301340503ba9815e48209050894b2b8dc9
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-13 20:55:03 +0100

    update: added the logix to handle pgo periods

--- core/src/generate_contract_details.py
+++ core/src/generate_contract_details.py
@@ -65,28 +65,46 @@ def generate_contract_details(value: int, con_type: int, sts_hist: dict):
         elif i == 2:
             if randint(0, 9) < 4:
                 for _ in range(i):
-                    contracts.append(con_period(periods[0], weights[0]))
+                    contracts.append(con_period(periods[1], weights[0]))
             else:
-                contracts.append(con_period(periods[1], weights[1]))
+                contracts.append(con_period(periods[2], weights[1]))
+        elif i == 0:
+            if len(contracts) == 0:
+                contracts.append(
+                    {
+                        "contract_title": "pgo",
+                        "num_of_sims": 1,
+                        "num_of_devices": 0,
+                        "con_period": 0,
+                        "devices": 0,
+                        "price": 0,
+                        "available_data": {
+                            "calls_times": 0,
+                            "cellular_data": 0.0,
+                            "roam_data": 0.0,
+                            "roam_call_time": 0,
+                        },
+                    }
+                )
         # period is more than 24 months
         else:
             while i != 0:
                 rand_num = randint(0, 9)
                 if i >= 3:
                     if rand_num == 0:
-                        contracts.append(con_period(periods[0], weights[0]))
+                        contracts.append(con_period(periods[1], weights[0]))
                         i -= 1
                     elif rand_num < 3:
-                        contracts.append(con_period(periods[1], weights[1]))
+                        contracts.append(con_period(periods[2], weights[1]))
                         i -= 2
                     else:
-                        contracts.append(con_period(periods[2], weights[2]))
+                        contracts.append(con_period(periods[3], weights[2]))
                         i -= 3
                 elif i == 2:
-                    contracts.append(con_period(periods[1], weights[1]))
+                    contracts.append(con_period(periods[2], weights[1]))
                     i -= 2
                 else:
-                    contracts.append(con_period(periods[0], weights[0]))
+                    contracts.append(con_period(periods[1], weights[0]))
                     i -= 1
     if len(contracts) == 0:
         contracts.append(
