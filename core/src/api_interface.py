b94c35c0d3e11ab7b33f2ec5945d55e5dde5357b
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-17 19:55:42 +0100

    feat: created an api to act as the intrface to insert customers

---
+++ core/src/api_interface.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from core.src.main import main
+
+app = FastAPI()
+
+@app.get("/healthcheck")
+def get_healthcheck():
+    return {"status": "The server is running"}
+
+
+@app.get("/create_records")
+def get_create_records(num_records: int | None =1):
+    if num_records == 0:
+        return JSONResponse(content={"status": "Failed"}, status_code= 400)
+    
+    main(num_records)
+    return {"status": "Ok", "records_created": num_records}
