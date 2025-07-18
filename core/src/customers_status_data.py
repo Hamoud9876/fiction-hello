fb808c7d5e1d659e7105549cd3ad24ded5cbc248
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-18 23:53:08 +0100

    update: added error handling

--- core/src/customers_status_data.py
+++ core/src/customers_status_data.py
@@ -3,6 +3,8 @@ from random import randint
 from core.src.cust_hist_active import cust_hist_active
 from core.src.cust_hist_inactive import cust_hist_inactive
 from core.src.cust_hist_long_changes import cust_hist_long_changes
+from core.exceptions.invalid_input_exception import InvalidInput
+import inspect
 from datetime import date
 
 
@@ -12,16 +14,16 @@ def customers_status_data(join_date: date):
     -----------------------------------------
     args: "join_date" represent the date of which
     the customer joined the orgnization
-    "cust_id" customer id
     -----------------------------------------
     return: a dict containing the most recent status
     along with the status history if exists
     """
 
     if not isinstance(join_date, date):
-        return "Not a valid input"
-    rand_num = randint(0, 9)
+        func_name = inspect.currentframe().f_code.co_name
+        raise InvalidInput(f"Invalid input in function '{func_name}': date")
 
+    rand_num = randint(0, 9)
     # customer active since joining
     if rand_num < 3:
         return cust_hist_active(customers_status, join_date)
