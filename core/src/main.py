0eaa5bee9efdcb833d47493fb5e098037fc06032
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-30 22:12:30 +0100

    update: added try and except block with loggin in the except

--- core/src/main.py
+++ core/src/main.py
@@ -5,19 +5,23 @@ from core.src.generate_contracts import generate_contracts
 from core.src.generate_sim_data import generate_sims_data
 from core.database.core_tables import create_tables
 from core.database.insert_tables import insert_tables
+from core.exceptions.invalid_input_exception import InvalidInput, logging
 
 
 def main(num_cust):
     customers = {"customers": generate_customers_data(num_cust)}
-
+    
     for i in customers["customers"]:
-        i["cust_status"] = customers_status_data(i["join_date"])
-        i["cust_address"] = generate_address_data()
-        i["con_details"] = generate_contracts(1, 1, i["cust_status"])
-        sims_num = 0
-        for y in i["con_details"]:
-            if y["num_of_sims"] > sims_num:
-                sims_num = y["num_of_sims"]
-        i["sims"] = generate_sims_data(sims_num)
+        try:
+            i["cust_status"] = customers_status_data(i["join_date"])
+            i["cust_address"] = generate_address_data()
+            i["con_details"] = generate_contracts(1, 1, i["cust_status"])
+            sims_num = 0
+            for y in i["con_details"]:
+                if y["num_of_sims"] > sims_num:
+                    sims_num = y["num_of_sims"]
+            i["sims"] = generate_sims_data(sims_num)
+        except InvalidInput as e:
+            logging.error(str(e.value))
     create_tables()
     insert_tables(customers)
