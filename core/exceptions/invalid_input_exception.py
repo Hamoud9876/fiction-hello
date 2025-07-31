4bfe99b8b2a4f9d36dba9a95c2b4f2f9e40e29af
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-31 23:05:45 +0100

    update:removed the logging

--- core/exceptions/invalid_input_exception.py
+++ core/exceptions/invalid_input_exception.py
@@ -1,12 +1,4 @@
 import inspect
-import logging
-
-
-logging.basicConfig(
-    filename="app_errors.log",
-    level=logging.ERROR,
-    format="%(asctime)s - %(levelname)s - %(message)s",
-)
 
 
 class InvalidInput(Exception):
