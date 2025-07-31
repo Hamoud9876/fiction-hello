1baf28be7d6bb670271540fd045703e8a695cd3c
Hamoud9876 <h.ghaleb95@gmail.com>
2025-07-31 23:01:25 +0100

    update: updated the tests to acount to changes in the function

--- core/test/test_generate_bellings.py
+++ core/test/test_generate_bellings.py
@@ -9,7 +9,7 @@ import pytest
 class TestGeneratBillings:
     def test_handles_wrong_input(self):
         with pytest.raises(InvalidInput) as e:
-            generate_bellings({})
+            generate_bellings({},2)
         assert "Invalid input in 'generate_bellings': period" in str(e.value)
 
     def test_generat_one_bill(self):
@@ -22,7 +22,7 @@ class TestGeneratBillings:
             "start_date": my_date,   
             "end_date": my_date +relativedelta(months=1)
         }]
-        result = generate_bellings(usage)
+        result = generate_bellings(usage,20)
 
         assert "amount" in result[0]
         assert "status" in result[0]
@@ -48,7 +48,7 @@ class TestGeneratBillings:
             "end_date": my_date +relativedelta(months=2)
         },]
 
-        result = generate_bellings(usage)
+        result = generate_bellings(usage,20)
 
         for i in result:
             assert "amount" in result[0]
@@ -59,7 +59,7 @@ class TestGeneratBillings:
             assert isinstance(i["amount"],float)
             assert isinstance(i["status"], str)
             assert isinstance(i["issue_date"], date)
-            assert isinstance(i["complete_date"], date)
+            assert isinstance(i["complete_date"], (date,None))
             assert isinstance(i["due_date"], date)
 
     def test_doesnot_generate_future_bills(self):
@@ -80,7 +80,7 @@ class TestGeneratBillings:
             "end_date": my_date +relativedelta(months=2)
         },]
 
-        result = generate_bellings(usage)
+        result = generate_bellings(usage,20)
 
         assert len(result) <2
 
