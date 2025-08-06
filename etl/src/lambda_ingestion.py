9a9279aece99a1a5e013599ec449a12165295796
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-06 17:39:17 +0100

    update: added th logic to retrieve the tables columns name and content and put them in S3

--- etl/src/lambda_ingestion.py
+++ etl/src/lambda_ingestion.py
@@ -2,6 +2,8 @@ from etl.database.db_connection import db_connection
 from etl.utils.check_bucket_content import check_bucket_content
 from etl.utils.insert_into_bucket import insert_into_bucket
 from datetime import datetime, timedelta
+import io
+import csv
 
 
 def ingest_data(event, context):
@@ -21,15 +23,34 @@ def ingest_data(event, context):
     half_hour_before = datetime.now() - timedelta(minutes=30)
     time_now = datetime.now()
     
-    for name in tables:
+    for table in tables:
+        #retrieving table content
         query_retrieve_data = f"""
         SELECT * 
-        FROM {name[0]}
+        FROM {table[0]}
         WHERE last_updated >= {half_hour_before 
          if check_bucket_content(bucket_name) else time_now}
         """
 
-        print(query_retrieve_data)
+        table_content = conn.run(query_retrieve_data)
+
+        #retrieving table columns headers
+        table_headers = conn.run(
+                f"""SELECT column_name
+                FROM information_schema.columns
+                WHERE table_schema = 'public'
+                AND table_name = '{table}';"""
+            )
+
+        #saving the content of the output in memory 
+        #and tuning it to CSV format
+        buffer = io.StringIO()
+        writer = csv.write(buffer)
+        writer.writerow(table_headers)
+        writer.writerows(table_content)
+        csv_content = buffer.getvalue()
+        insert_into_bucket(bucket_name,table, csv_content)
+        
 
 
     
