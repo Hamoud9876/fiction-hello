51886571791003cd3b31de76d600f87a4c545fdf
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-07 18:02:13 +0100

    update: added subnet_id for aws_instance.web

--- core/terraform/ec2.tf
+++ core/terraform/ec2.tf
@@ -17,6 +17,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  subnet_id = aws_subnet.public1.id
   vpc_security_group_ids = [aws_security_group.allow_internet_traffic.id]
   key_name = "hamoud-key"
   user_data_replace_on_change = true
