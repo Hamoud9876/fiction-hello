109bf8efda22c21eb5f4a4f36c15e0c98d005488
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-07 18:40:30 +0100

    update: changed the directory for the file from rtl to core

---
+++ core/database/db_connection_olap.py
@@ -0,0 +1,19 @@
+from pg8000.native import Connection
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
+
+
+def db_connection():
+    return Connection(
+        user=os.getenv("DB_OLAP_USERNAME"),
+        password=os.getenv("DB_OLAP_PASSWORD"),
+        database=os.getenv("DB_OLAP_DATABASE"),
+        host=os.getenv("DB_OLAP_HOST"),
+        port=os.getenv("DB_OLAP_PORT"),
+    )
+
+
+def close_db(conn):
+    conn.close()
