ae2f5a290cb55f98c10875b26098ff9ee4ba8618
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-07 18:05:40 +0100

    update: created a custom vpc and new public and private subnets to host my ec2 and 2 rds

--- core/terraform/vpc.tf
+++ core/terraform/vpc.tf
@@ -1,8 +1,78 @@
+#custom VPN
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = { Name = "custom-vpc" }
+}
+
+#public1 subnet for the EC2
+resource "aws_subnet" "public1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-2a"
+  tags = { Name = "public1-subnet" }
+}
+
+#public2 subnet for the EC2
+resource "aws_subnet" "public2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "eu-west-2b"
+  tags = { Name = "public2-subnet" }
+}
+
+#private1 subnet for the for core DB
+resource "aws_subnet" "private1" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "eu-west-2a"
+  tags = { Name = "private1-subnet" }
+}
+
+#private2 subnet for the for core DB
+resource "aws_subnet" "private2" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.4.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "eu-west-2b"
+  tags = { Name = "private2-subnet" }
+}
+
+#internet gateway to allow internet access to 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "main-igw" }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public1" {
+  subnet_id = aws_subnet.public1.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public2" {
+  subnet_id      = aws_subnet.public2.id
+  route_table_id = aws_route_table.public.id
+}
+
+
 
 resource "aws_security_group" "allow_internet_traffic" {
   name="allow-internet-traffic"
   description = "allow inbound access to the ec2"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = aws_vpc.main.id
 
   tags = {
     Name = "ec2_sg"
@@ -37,33 +107,10 @@ resource "aws_vpc_security_group_egress_rule" "allow_internet_traffic_engress" {
 }
 
 
-data "aws_vpc" "default" {
-  tags = {
-    Name = "Default VPC"
-  }
-}
-
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-resource "aws_db_subnet_group" "default" {
-  name       = "fiction-db-subnet-group"
-  subnet_ids = data.aws_subnets.default.ids
-
-  tags = {
-    Name = "fiction-db-subnet-group"
-  }
-}
-
-
 resource "aws_security_group" "rds_sg" {
   name="allow-rds-traffic"
   description = "allow inbound access to rds"
-  vpc_id      = data.aws_vpc.default.id
+  vpc_id      = aws_vpc.main.id
 
   tags = {
     Name = "rds_sg"
@@ -86,3 +133,40 @@ resource "aws_vpc_security_group_egress_rule" "allow_rds_engress" {
   ip_protocol = "-1"
 }
 
+resource "aws_security_group_rule" "allow_internet_access" {
+  type                     = "ingress"
+  from_port                = 5432
+  to_port                  = 5432
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.rds_sg_olap.id
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_db_subnet_group" "private" {
+  name       = "my-db-subnet-group-private"
+  subnet_ids = [aws_subnet.private1.id, aws_subnet.private2.id] 
+
+  tags = {
+    Name = "My db Subnet Group"
+  }
+}
+
+
+resource "aws_security_group" "rds_sg_olap" {
+  name="allow-rds-traffic-olap"
+  description = "allow exterenal access to rds"
+  vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "rds_sg-olap"
+  }
+}
+
+resource "aws_db_subnet_group" "public" {
+  name       = "my-db-subnet-group"
+  subnet_ids = [aws_subnet.public1.id, aws_subnet.public2.id] 
+
+  tags = {
+    Name = "My db Subnet Group"
+  }
+}
