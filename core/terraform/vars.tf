bfb6c484017bc4b80a0fb2793e8edc28fa86e19d
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-07 18:42:27 +0100

    update: added the new variables for the olap db

--- core/terraform/vars.tf
+++ core/terraform/vars.tf
@@ -11,7 +11,25 @@ variable "db_user"{
 }
 
 variable "db_password"{
-    description = "Passeord for the master user for RDS DB"
+    description = "Password for the master user for RDS DB"
+    type = string
+    default = "password"
+}
+
+variable "db_name_olap" {
+    description = "Name of the RDS DB"
+    type = string
+    default = "db_name"
+}
+
+variable "db_user_olap"{
+    description = "Master user of the RDS DB"
+    type = string
+    default = "user"
+}
+
+variable "db_password_olap"{
+    description = "Password for the master user for RDS DB"
     type = string
     default = "password"
 }
