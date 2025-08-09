b3eb790ba514a57bb07e246fba2e60bed9ee89b9
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-09 23:25:55 +0100

    update: ran with flake8 and black

--- core/src/customer_usage.py
+++ core/src/customer_usage.py
@@ -1,8 +1,9 @@
-from datetime import date,datetime
+from datetime import date, datetime
 from core.exceptions.invalid_input_exception import InvalidInput
 from random import uniform, randint
 from dateutil.relativedelta import relativedelta
 import inspect
+
 
 def customer_usage(period: int, start_date: date):
     """
@@ -18,28 +19,31 @@ def customer_usage(period: int, start_date: date):
     representing the customer usage for each month
     """
     func_name = inspect.currentframe().f_code.co_name
-    if not isinstance(period, int) or period not in [12,24,36]:
+    if not isinstance(period, int) or period not in [12, 24, 36]:
         raise InvalidInput(f"Invalid input in '{func_name}': period")
-    
-    if not isinstance(start_date, (date,datetime)):
+
+    if not isinstance(start_date, (date, datetime)):
         raise InvalidInput(f"Invalid input in '{func_name}': start_date")
     usage_lst = []
     for i in range(period):
-        usage_lst.append({
-            "used_cellular_data": (round(uniform(30, 150.00),2) 
-                                    if randint(0,20) != 0 
-                                    else round(uniform(145.00, 200.00),2)),
-            "used_call_time": (randint(30, 150) if randint(0,20) != 0
-                               else randint(140, 180)),
-            "used_roam_data": (round(uniform(0.00, 50.00),2) 
-                               if randint(0,20) != 0
-                               else 0.00),
-            "used_roam_call_time": (randint(0, 50)
-                                    if randint(0,20) != 0
-                                    else 0),
-            "start_date": (usage_lst[-1]["start_date"]
-                           if len(usage_lst) >0
-                           else start_date),
-            "end_date": start_date +relativedelta(months=i)
-        })
+        usage_lst.append(
+            {
+                "used_cellular_data": (
+                    round(uniform(30, 150.00), 2)
+                    if randint(0, 20) != 0
+                    else round(uniform(145.00, 200.00), 2)
+                ),
+                "used_call_time": (
+                    randint(30, 150) if randint(0, 20) != 0 else randint(140, 180)
+                ),
+                "used_roam_data": (
+                    round(uniform(0.00, 50.00), 2) if randint(0, 20) != 0 else 0.00
+                ),
+                "used_roam_call_time": (randint(0, 50) if randint(0, 20) != 0 else 0),
+                "start_date": (
+                    usage_lst[-1]["start_date"] if len(usage_lst) > 0 else start_date
+                ),
+                "end_date": start_date + relativedelta(months=i),
+            }
+        )
     return usage_lst
