1d5287db85a0e1a6467330e214241864ef7bc044
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-09 23:27:12 +0100

    update: ran with flake8 and black

--- core/src/generate_contracts.py
+++ core/src/generate_contracts.py
@@ -41,8 +41,8 @@ def generate_contracts(value: int, con_type: int, sts_hist: dict):
 
     for y in range(lst_len):
         if sts_hist["cust_sts_hist"][y]["customer_status"] == "Active":
-            #checking if there is a status change after this one
-            #and returning the period difference between them
+            # checking if there is a status change after this one
+            # and returning the period difference between them
             if y + 1 < lst_len:
                 period = (
                     int(
@@ -54,13 +54,15 @@ def generate_contracts(value: int, con_type: int, sts_hist: dict):
                 ) // 365
 
                 if period >= 1:
-                    years_periods["periods"].append({
-                        "period": period,
-                        "start_date": sts_hist["cust_sts_hist"][y]["change_date"]
-                        })
-                    
-            #last change in history
-            #returns the diff between the date of the change until today
+                    years_periods["periods"].append(
+                        {
+                            "period": period,
+                            "start_date": sts_hist["cust_sts_hist"][y]["change_date"],
+                        }
+                    )
+
+            # last change in history
+            # returns the diff between the date of the change until today
             else:
                 period = (
                     int(
@@ -72,24 +74,28 @@ def generate_contracts(value: int, con_type: int, sts_hist: dict):
                 ) // 365
 
                 if period >= 1:
-                    years_periods["periods"].append({
-                        "period": period,
-                        "start_date": sts_hist["cust_sts_hist"][y]["change_date"]
-                        })
+                    years_periods["periods"].append(
+                        {
+                            "period": period,
+                            "start_date": sts_hist["cust_sts_hist"][y]["change_date"],
+                        }
+                    )
 
     for i in years_periods["periods"]:
         # period of 12 months
         if i["period"] == 1:
-            contracts.append(con_details(12, 1.3,i["start_date"]))
+            contracts.append(con_details(12, 1.3, i["start_date"]))
 
         # period of 24 months
         elif i["period"] == 2:
             if randint(0, 9) < 4:
                 for x in range(len(i)):
-                    effective_date = i["start_date"] +relativedelta(months=x*12)
-                    contracts.append(con_details(periods[1], weights[0],effective_date))
+                    effective_date = i["start_date"] + relativedelta(months=x * 12)
+                    contracts.append(
+                        con_details(periods[1], weights[0], effective_date)
+                    )
             else:
-                contracts.append(con_details(periods[2], weights[1],i["start_date"]))
+                contracts.append(con_details(periods[2], weights[1], i["start_date"]))
         # elif i["period"] == 0:
         #     if len(contracts) == 0:
         #         contracts.append(
@@ -111,51 +117,61 @@ def generate_contracts(value: int, con_type: int, sts_hist: dict):
         # period is more than 24 months
         else:
             effective_date = i["start_date"]
-            #splitting the period into multiple contracts
+            # splitting the period into multiple contracts
             while i["period"] != 0:
                 rand_num = randint(0, 9)
                 if i["period"] >= 3:
 
-                    #%10 chance to create a 1 year contract when the total years is 3 or more
+                    # %10 chance to create a 1 year contract when the total years is 3 or more
                     if rand_num == 0:
-                        effective_date = effective_date +relativedelta(months=12)
-                        contracts.append(con_details(periods[1], weights[0], effective_date))
+                        effective_date = effective_date + relativedelta(months=12)
+                        contracts.append(
+                            con_details(periods[1], weights[0], effective_date)
+                        )
                         i["period"] -= 1
 
-                    #%20 chance to create a 2 year contract when the total years is 3 or more
+                    # %20 chance to create a 2 year contract when the total years is 3 or more
                     elif rand_num < 3:
-                        effective_date = effective_date +relativedelta(months=24)
-                        contracts.append(con_details(periods[2], weights[1], effective_date))
+                        effective_date = effective_date + relativedelta(months=24)
+                        contracts.append(
+                            con_details(periods[2], weights[1], effective_date)
+                        )
                         i["period"] -= 2
 
-
-                    #%70 chance to create a 3 year contract when the total years is 3 or more
+                    # %70 chance to create a 3 year contract when the total years is 3 or more
                     else:
-                        effective_date = effective_date +relativedelta(months=36)
-                        contracts.append(con_details(periods[3], weights[2], effective_date))
+                        effective_date = effective_date + relativedelta(months=36)
+                        contracts.append(
+                            con_details(periods[3], weights[2], effective_date)
+                        )
                         i["period"] -= 3
 
                 elif i["period"] == 2:
-                    #%30 chance to create a 1 year contract when the total years is 2 or more
-                    if rand_num <3:
-                        effective_date = effective_date +relativedelta(months=12)
-                        contracts.append(con_details(periods[1], weights[0], effective_date))
+                    # %30 chance to create a 1 year contract when the total years is 2 or more
+                    if rand_num < 3:
+                        effective_date = effective_date + relativedelta(months=12)
+                        contracts.append(
+                            con_details(periods[1], weights[0], effective_date)
+                        )
                         i["period"] -= 1
 
                     else:
-                        #%70 chance to create a 2 year contract when the total years is 2 or more
-                        effective_date = effective_date +relativedelta(months=24)
-                        contracts.append(con_details(periods[2], weights[1], effective_date))
+                        # %70 chance to create a 2 year contract when the total years is 2 or more
+                        effective_date = effective_date + relativedelta(months=24)
+                        contracts.append(
+                            con_details(periods[2], weights[1], effective_date)
+                        )
                         i["period"] -= 2
 
                 else:
-                    #creating a one year contract if the total years is 1
-                    effective_date = effective_date +relativedelta(months=12)
-                    contracts.append(con_details(periods[1], weights[0], effective_date))
+                    # creating a one year contract if the total years is 1
+                    effective_date = effective_date + relativedelta(months=12)
+                    contracts.append(
+                        con_details(periods[1], weights[0], effective_date)
+                    )
                     i["period"] -= 1
 
-
-    #if the customer total join period was less than a year                
+    # if the customer total join period was less than a year
     if len(contracts) == 0:
         contracts.append(
             {
@@ -171,8 +187,8 @@ def generate_contracts(value: int, con_type: int, sts_hist: dict):
                     "roam_data": 0.0,
                     "roam_call_time": 0,
                 },
-                "start_date": sts_hist["cust_sts_hist"][0]["change_date"]
+                "start_date": sts_hist["cust_sts_hist"][0]["change_date"],
             }
         )
-        
+
     return contracts
