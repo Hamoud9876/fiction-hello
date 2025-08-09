f0f80180ccd8dbb2dbb33db26c9271fcaa952f69
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-09 23:28:12 +0100

    update: ran with flake8 and black

--- core/test/test_customer_usage.py
+++ core/test/test_customer_usage.py
@@ -1,7 +1,8 @@
 from core.src.customer_usage import customer_usage
 from core.exceptions.invalid_input_exception import InvalidInput
-from datetime import datetime,date
+from datetime import datetime, date
 import pytest
+
 
 class TestCustomerUsage:
     def test_handles_wrong_input(self):
@@ -9,24 +10,22 @@ class TestCustomerUsage:
         date_obj = datetime.strptime(date, "%d/%m/%Y")
         date_obj = date_obj.date()
         with pytest.raises(InvalidInput) as e:
-            customer_usage("12",date_obj)
+            customer_usage("12", date_obj)
         assert "Invalid input in 'customer_usage': period" in str(e.value)
         with pytest.raises(InvalidInput) as e:
-            customer_usage(13,date_obj)
+            customer_usage(13, date_obj)
         assert "Invalid input in 'customer_usage': period" in str(e.value)
 
-
         with pytest.raises(InvalidInput) as e:
-            customer_usage(12,date)
+            customer_usage(12, date)
         assert "Invalid input in 'customer_usage': start_date" in str(e.value)
-
 
     def test_generate_12_months_record(self):
         my_date = "10/2/2025"
         date_obj = datetime.strptime(my_date, "%d/%m/%Y")
         date_obj = date_obj.date()
 
-        result = customer_usage(12,date_obj)
+        result = customer_usage(12, date_obj)
 
         for i in result:
             assert "used_cerllular_data" in i
@@ -41,13 +40,13 @@ class TestCustomerUsage:
             assert isinstance(i["start_date"], date)
             assert "end_date" in i
             assert isinstance(i["end_date"], date)
-    
+
     def test_generate_24_months_record(self):
         my_date = "10/2/2025"
         date_obj = datetime.strptime(my_date, "%d/%m/%Y")
         date_obj = date_obj.date()
 
-        result = customer_usage(24,date_obj)
+        result = customer_usage(24, date_obj)
 
         for i in result:
             assert "used_cerllular_data" in i
@@ -62,15 +61,13 @@ class TestCustomerUsage:
             assert isinstance(i["start_date"], date)
             assert "end_date" in i
             assert isinstance(i["end_date"], date)
-
-
 
     def test_generate_36_months_record(self):
         my_date = "10/2/2025"
         date_obj = datetime.strptime(my_date, "%d/%m/%Y")
         date_obj = date_obj.date()
 
-        result = customer_usage(36,date_obj)
+        result = customer_usage(36, date_obj)
 
         for i in result:
             assert "used_cerllular_data" in i
@@ -85,4 +82,3 @@ class TestCustomerUsage:
             assert isinstance(i["start_date"], date)
             assert "end_date" in i
             assert isinstance(i["end_date"], date)
-
--- core/test/test_generate_bellings.py
+++ core/test/test_generate_bellings.py
@@ -9,20 +9,22 @@ import pytest
 class TestGeneratBillings:
     def test_handles_wrong_input(self):
         with pytest.raises(InvalidInput) as e:
-            generate_bellings({},2)
+            generate_bellings({}, 2)
         assert "Invalid input in 'generate_bellings': period" in str(e.value)
 
     def test_generat_one_bill(self):
-        my_date = date(day=1, month=1,year=2025)
-        usage = [{
-            "used_cellular_data": 155.00,
-            "used_call_time": 155,
-            "used_roam_data": 55.00,
-            "used_roam_call_time": 55,
-            "start_date": my_date,   
-            "end_date": my_date +relativedelta(months=1)
-        }]
-        result = generate_bellings(usage,20)
+        my_date = date(day=1, month=1, year=2025)
+        usage = [
+            {
+                "used_cellular_data": 155.00,
+                "used_call_time": 155,
+                "used_roam_data": 55.00,
+                "used_roam_call_time": 55,
+                "start_date": my_date,
+                "end_date": my_date + relativedelta(months=1),
+            }
+        ]
+        result = generate_bellings(usage, 20)
 
         assert "amount" in result[0]
         assert "status" in result[0]
@@ -31,24 +33,27 @@ class TestGeneratBillings:
         assert "due_date" in result[0]
 
     def test_generate_multiple_bills(self):
-        my_date = date(day=1, month=1,year=2025)
-        usage = [{
-            "used_cellular_data": 155.00,
-            "used_call_time": 155,
-            "used_roam_data": 55.00,
-            "used_roam_call_time": 55,
-            "start_date": my_date,   
-            "end_date": my_date +relativedelta(months=1)
-        },{
-            "used_cellular_data": 155.00,
-            "used_call_time": 155,
-            "used_roam_data": 55.00,
-            "used_roam_call_time": 55,
-            "start_date": my_date+relativedelta(months=1),   
-            "end_date": my_date +relativedelta(months=2)
-        },]
+        my_date = date(day=1, month=1, year=2025)
+        usage = [
+            {
+                "used_cellular_data": 155.00,
+                "used_call_time": 155,
+                "used_roam_data": 55.00,
+                "used_roam_call_time": 55,
+                "start_date": my_date,
+                "end_date": my_date + relativedelta(months=1),
+            },
+            {
+                "used_cellular_data": 155.00,
+                "used_call_time": 155,
+                "used_roam_data": 55.00,
+                "used_roam_call_time": 55,
+                "start_date": my_date + relativedelta(months=1),
+                "end_date": my_date + relativedelta(months=2),
+            },
+        ]
 
-        result = generate_bellings(usage,20)
+        result = generate_bellings(usage, 20)
 
         for i in result:
             assert "amount" in result[0]
@@ -56,32 +61,33 @@ class TestGeneratBillings:
             assert "issue_date" in result[0]
             assert "complete_date" in result[0]
             assert "due_date" in result[0]
-            assert isinstance(i["amount"],float)
+            assert isinstance(i["amount"], float)
             assert isinstance(i["status"], str)
             assert isinstance(i["issue_date"], date)
-            assert isinstance(i["complete_date"], (date,None))
+            assert isinstance(i["complete_date"], (date, None))
             assert isinstance(i["due_date"], date)
 
     def test_doesnot_generate_future_bills(self):
-        my_date = date(day=1, month=7,year=2025)
-        usage = [{
-            "used_cellular_data": 155.00,
-            "used_call_time": 155,
-            "used_roam_data": 55.00,
-            "used_roam_call_time": 55,
-            "start_date": my_date,   
-            "end_date": my_date +relativedelta(months=1)
-        },{
-            "used_cellular_data": 155.00,
-            "used_call_time": 155,
-            "used_roam_data": 55.00,
-            "used_roam_call_time": 55,
-            "start_date": my_date+relativedelta(months=1),   
-            "end_date": my_date +relativedelta(months=2)
-        },]
+        my_date = date(day=1, month=7, year=2025)
+        usage = [
+            {
+                "used_cellular_data": 155.00,
+                "used_call_time": 155,
+                "used_roam_data": 55.00,
+                "used_roam_call_time": 55,
+                "start_date": my_date,
+                "end_date": my_date + relativedelta(months=1),
+            },
+            {
+                "used_cellular_data": 155.00,
+                "used_call_time": 155,
+                "used_roam_data": 55.00,
+                "used_roam_call_time": 55,
+                "start_date": my_date + relativedelta(months=1),
+                "end_date": my_date + relativedelta(months=2),
+            },
+        ]
 
-        result = generate_bellings(usage,20)
+        result = generate_bellings(usage, 20)
 
-        assert len(result) <2
-
-
+        assert len(result) < 2
