4426986eb1093cef4f4725448e55d5c11b6cf684
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-09 23:30:47 +0100

    feat: vars files for terraform

---
+++ etl/terraform/vars.tf
@@ -0,0 +1,29 @@
+variable "DB_OLAP_USERNAME" {
+  type = string
+  default = "User"
+}
+
+
+variable "DB_OLAP_PASSWORD" {
+  type = string
+  default = "password"
+}
+
+
+variable "DB_OLAP_DATABASE" {
+  type = string
+  default = "db_name"
+}
+
+
+variable "DB_OLAP_HOST" {
+  type = string
+  default = "host"
+}
+
+
+variable "DB_OLAP_PORT" {
+  type = string
+  default = "port"
+}
+
