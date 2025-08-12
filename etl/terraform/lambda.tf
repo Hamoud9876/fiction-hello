b343edf14cf2317b799173b98b8f5a8859682c98
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-12 21:18:05 +0100

    update: increated the timeout time

--- etl/terraform/lambda.tf
+++ etl/terraform/lambda.tf
@@ -13,8 +13,11 @@ resource "aws_lambda_function" "ingestion_lambda" {
   role             = aws_iam_role.lambda_role.arn
   handler          = "lambda_ingestion.ingest_data"
   runtime          = "python3.10"
+  timeout          = 521
 
-  layers =  [aws_lambda_layer_version.ingestions_layers_dep.arn, aws_lambda_layer_version.ingestions_layers_packages.arn]
+
+  layers =  [aws_lambda_layer_version.ingestion_layer.arn]
+ 
 
   environment {
     variables = {
