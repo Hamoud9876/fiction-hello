a33d6816f0c3bf67cbb9c319e064f077f4bd4d05
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-12 21:19:07 +0100

    update: changed the structure of the files that are achived to make it clear to navigate

--- etl/terraform/layer.tf
+++ etl/terraform/layer.tf
@@ -1,39 +1,33 @@
-#creating a directory and installing the ingestion lambda requirements into it
-resource "null_resource" "create_packages" {
+# Create a build directory with correct Lambda layer structure
+resource "null_resource" "build_ingestion_layer" {
   provisioner "local-exec" {
     command = <<EOT
-    mkdir -p ${path.module}/../packages_ingestion/python
-    pip install -r ${path.module}/../src/lambda_ingestion/requirement_ingestion.txt -t ${path.module}/../packages_ingestion/python
-    EOT
-  }
+      set -e
 
-    triggers = {
-    utils_hash = sha1(join("", [
-      filemd5("${path.module}/../src/lambda_ingestion/requirement_ingestion.txt"),
-      timestamp()
-    ]))
-  }
-}
+      # Remove old build
+      rm -rf ${path.module}/../build_ingestion_layer
+      mkdir -p ${path.module}/../build_ingestion_layer/python
 
+      # Install dependencies into python/
+      pip install -r ${path.module}/../src/lambda_ingestion/requirement_ingestion.txt \
+        -t ${path.module}/../build_ingestion_layer/python
 
-#orgnizing the files ingestion lambda depends on in a new directory 
-#compatable with AWS file structure
-resource "null_resource" "utils_ingestion_dep" {
-  provisioner "local-exec" {
-    command = <<EOT
-      mkdir -p "${path.module}/../utils_ingestion_dep/python/etl/utils"
-      ls -l "${path.module}/../utils"
-      cp "${path.module}/../utils/check_bucket_content.py" \
-         "${path.module}/../utils/insert_into_bucket.py" \
-         "${path.module}/../utils_ingestion_dep/python/etl/utils/"
-      mkdir -p "${path.module}/../utils_ingestion_dep/python/etl/database"
-      cp "${path.module}/../database/db_connection_olap.py" \
-         "${path.module}/../utils_ingestion_dep/python/etl/database/"
+      # Copy your ETL Python modules into python/ so imports work
+      mkdir -p ${path.module}/../build_ingestion_layer/python/etl/utils
+      mkdir -p ${path.module}/../build_ingestion_layer/python/etl/database
+
+      cp ${path.module}/../utils/check_bucket_content.py \
+         ${path.module}/../utils/insert_into_bucket.py \
+         ${path.module}/../build_ingestion_layer/python/etl/utils/
+
+      cp ${path.module}/../database/db_connection_olap.py \
+         ${path.module}/../build_ingestion_layer/python/etl/database/
     EOT
   }
 
   triggers = {
-    utils_hash = sha1(join("", [
+    hash = sha1(join("", [
+      filemd5("${path.module}/../src/lambda_ingestion/requirement_ingestion.txt"),
       filemd5("${path.module}/../utils/check_bucket_content.py"),
       filemd5("${path.module}/../utils/insert_into_bucket.py"),
       filemd5("${path.module}/../database/db_connection_olap.py"),
@@ -42,38 +36,18 @@ resource "null_resource" "utils_ingestion_dep" {
   }
 }
 
-
-#zipping the ingestion lambda dependencies files
-data "archive_file" "ingestion_dep" {
+# Zip it up
+data "archive_file" "ingestion_layer_zip" {
   type        = "zip"
-  source_dir = "${path.module}/../utils_ingestion_dep"
-  output_path = "${path.module}/../archives/lambda_ingestion/ingestion_dep.zip"
-  depends_on = [ null_resource.utils_ingestion_dep ]
+  source_dir  = "${path.module}/../build_ingestion_layer"
+  output_path = "${path.module}/../archives/lambda_ingestion/ingestion.zip"
+  depends_on  = [null_resource.build_ingestion_layer]
 }
 
-
-#creating the layer for the ingestion dependencies files
-resource "aws_lambda_layer_version" "ingestions_layers_dep" {
-  filename   = "${path.module}/../archives/lambda_ingestion/ingestion_dep.zip"
-  layer_name = "ingestion_dep"
-  depends_on = [data.archive_file.ingestion_dep]
+# Create the Lambda layer
+resource "aws_lambda_layer_version" "ingestion_layer" {
+  filename   = data.archive_file.ingestion_layer_zip.output_path
+  layer_name = "ingestion_combined_layer"
+  compatible_runtimes = ["python3.12"]
+  depends_on = [data.archive_file.ingestion_layer_zip]
 }
-
-
-#zipping the ingestion lambda packages files
-data "archive_file" "ingestion_packages" {
-  type        = "zip"
-  source_dir = "${path.module}/../packages_ingestion"
-  output_path = "${path.module}/../archives/lambda_ingestion/packages_ingestion.zip"
-  depends_on = [ null_resource.create_packages ]
-}
-
-
-#creating the layer for the ingestion dependencies files
-resource "aws_lambda_layer_version" "ingestions_layers_packages" {
-  filename   = "${path.module}/../archives/lambda_ingestion/packages_ingestion.zip"
-  layer_name = "ingestion_packages"
-  depends_on = [data.archive_file.ingestion_packages]
-}
-
-
