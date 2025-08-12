0aad407538dbcac83158ec96498132199e7452fb
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-12 21:14:33 +0100

    update: added created at colum to most tables

--- core/database/core_tables.py
+++ core/database/core_tables.py
@@ -7,7 +7,9 @@ def create_tables():
     query = """
     CREATE TABLE IF NOT EXISTS genders(
   gender_id SERIAL PRIMARY KEY,
-  gender_title varchar(200) UNIQUE
+  gender_title varchar(200) UNIQUE,
+  created_at timestamp,
+  last_updated timestamp
 );
 """
     conn.run(query)
@@ -15,14 +17,18 @@ def create_tables():
     query = """
     CREATE TABLE IF NOT EXISTS pronounce(
   pronounce_id SERIAL PRIMARY KEY,
-  pronounce_title varchar(100) UNIQUE
+  pronounce_title varchar(100) UNIQUE,
+  created_at timestamp,
+  last_updated timestamp
 );"""
     conn.run(query)
 
     query = """
     CREATE TABLE IF NOT  EXISTS customers_status(
             customer_status_id SERIAL PRIMARY KEY,
-            status varchar(30) UNIQUE
+            status varchar(30) UNIQUE,
+            created_at timestamp,
+            last_updated timestamp
             );
     """
     conn.run(query)
@@ -48,7 +54,9 @@ def create_tables():
 customer_status_history_id SERIAL PRIMARY KEY,
 customer_status_id int REFERENCES customers_status(customer_status_id),
 customer_id int REFERENCES customers(customer_id),
-change_date timestamp
+change_date timestamp,
+created_at timestamp,
+last_updated timestamp
 );
     """
     conn.run(query)
@@ -56,7 +64,9 @@ change_date timestamp
     query = """
     CREATE TABLE IF NOT EXISTS contract_types(
             contract_type_id SERIAL PRIMARY KEY,
-            contract_type varchar(100) UNIQUE
+            contract_type varchar(100) UNIQUE,
+            created_at timestamp,
+  last_updated timestamp
             )
     """
     conn.run(query)
@@ -64,7 +74,9 @@ change_date timestamp
     query = """
     CREATE TABLE IF NOT EXISTS contracts_periods(
         contract_period_id SERIAL PRIMARY KEY,
-        period int UNIQUE
+        period int UNIQUE,
+        created_at timestamp,
+  last_updated timestamp
        );
        """
     conn.run(query)
@@ -72,7 +84,9 @@ change_date timestamp
     query = """
 CREATE TABLE IF NOT EXISTS sims_validation(
 validation_id SERIAL PRIMARY KEY,
-validation varchar(20) UNIQUE
+validation varchar(20) UNIQUE,
+created_at timestamp,
+last_updated timestamp
 );
 """
     conn.run(query)
@@ -90,7 +104,9 @@ validation_id int REFERENCES sims_validation(validation_id),
 CREATE TABLE IF NOT EXISTS sim_valid_history(
 sim_valid_hist_id SERIAL PRIMARY KEY,
 validation_id int REFERENCES sims_validation(validation_id),
-change_date timestamp
+change_date timestamp,
+created_at timestamp,
+last_updated timestamp
 );
 """
     conn.run(query)
@@ -103,7 +119,9 @@ change_date timestamp
   avail_roam_data decimal(12,2),
   avail_roam_calls_time int,
   start_date timestamp,
-  end_date timestamp
+  end_date timestamp,
+  created_at timestamp,
+  last_updated timestamp
     );
 """
 
@@ -137,7 +155,9 @@ last_updated timestamp
     query = """CREATE TABLE IF NOT EXISTS customers_contracts(
             customer_contract_id SERIAL PRIMARY KEY,
             customer_id int REFERENCES customers(customer_id),
-            contract_id int REFERENCES contracts(contract_id)
+            contract_id int REFERENCES contracts(contract_id),
+            created_at timestamp,
+  last_updated timestamp
             );
     """
     conn.run(query)
@@ -145,14 +165,18 @@ last_updated timestamp
     query = """CREATE TABLE IF NOT EXISTS contract_details_sims(
         con_detail_sims_id SERIAL PRIMARY KEY,
 contract_details_id int REFERENCES contract_details(contract_details_id),
-        sim_id int REFERENCES sims(sim_id)
+        sim_id int REFERENCES sims(sim_id),
+        created_at timestamp,
+  last_updated timestamp
 );
 """
     conn.run(query)
 
     query = """CREATE TABLE IF NOT EXISTS devices_types(
             device_type_id SERIAL PRIMARY KEY,
-            device_type varchar(100) UNIQUE
+            device_type varchar(100) UNIQUE,
+            created_at timestamp,
+  last_updated timestamp
             );
     """
     conn.run(query)
@@ -163,7 +187,9 @@ contract_details_id int REFERENCES contract_details(contract_details_id),
             company varchar(100),
             screen_size decimal(4,1),
             ram int,
-            storage int
+            storage int,
+            created_at timestamp,
+  last_updated timestamp
             );
     """
     conn.run(query)
@@ -196,14 +222,18 @@ last_updated timestamp
     query = """CREATE TABLE IF NOT EXISTS contracts_details_devices(
   con_details_dev_id SERIAL PRIMARY KEY,
   contract_details_id int REFERENCES contract_details(contract_details_id),
-  device_id int REFERENCES devices(device_id)
+  device_id int REFERENCES devices(device_id),
+  created_at timestamp,
+  last_updated timestamp
 );"""
     conn.run(query)
 
     query = """CREATE TABLE IF NOT EXISTS contracts_details_sims(
   con_detail_sims_id SERIAL PRIMARY KEY,
   contract_details_id int REFERENCES contract_details(contract_details_id),
-  sim_id int REFERENCES sims(sim_id)
+  sim_id int REFERENCES sims(sim_id),
+  created_at timestamp,
+  last_updated timestamp
 )
 """
 
@@ -215,7 +245,9 @@ last_updated timestamp
             used_roam_data decimal(12,2),
             used_roam_call_time int,
             start_date timestamp,
-            end_date timestamp
+            end_date timestamp,
+            created_at timestamp,
+  last_updated timestamp
             );
     """
     conn.run(query)
@@ -223,14 +255,18 @@ last_updated timestamp
     query = """CREATE TABLE IF NOT EXISTS customers_sims(
             customer_sim_id SERIAL PRIMARY KEY,
             customer_id int REFERENCES customers(customer_id),
-            sim_id int REFERENCES sims(sim_id)
+            sim_id int REFERENCES sims(sim_id),
+            created_at timestamp,
+  last_updated timestamp
             );
     """
     conn.run(query)
 
     query = """CREATE TABLE IF NOT EXISTS address_type(
             address_type_id SERIAL PRIMARY KEY,
-            address_type varchar(30) UNIQUE
+            address_type varchar(30) UNIQUE,
+            created_at timestamp,
+  last_updated timestamp
             );
     """
     conn.run(query)
@@ -254,14 +290,18 @@ last_updated timestamp
     query = """CREATE TABLE IF NOT EXISTS customers_address(
             customers_address_id SERIAL PRIMARY KEY,
             customer_id int REFERENCES customers(customer_id),
-            address_id int REFERENCES address(address_id)
+            address_id int REFERENCES address(address_id),
+            created_at timestamp,
+  last_updated timestamp
             );
     """
     conn.run(query)
 
     query = """CREATE TABLE IF NOT EXISTS billing_status(
             bill_status_id SERIAL PRIMARY KEY,
-            status varchar(30) UNIQUE
+            status varchar(30) UNIQUE,
+            created_at timestamp,
+  last_updated timestamp
             );
     """
     conn.run(query)
@@ -274,7 +314,9 @@ last_updated timestamp
             bill_status_id int REFERENCES billing_status(bill_status_id),
             issue_date timestamp,
             completed_date timestamp,
-            due_date timestamp
+            due_date timestamp,
+            created_at timestamp,
+        last_updated timestamp
             );
     """
     conn.run(query)
@@ -283,7 +325,9 @@ last_updated timestamp
             billing_status_history_id SERIAL PRIMARY KEY,
             bill_id int REFERENCES billing(bill_id),
             bill_status_id int REFERENCES billing_status(bill_status_id),
-            change_date timestamp
+            change_date timestamp,
+            created_at timestamp,
+  last_updated timestamp
             );
     """
     conn.run(query)
@@ -295,7 +339,9 @@ last_updated timestamp
             roam_call_minute_rate decimal(5,2),
             roam_data_mp_rate decimal(5,2),
             start_date timestamp,
-            end_date timestamp
+            end_date timestamp,
+            created_at timestamp,
+  last_updated timestamp
             );
     """
     conn.run(query)
