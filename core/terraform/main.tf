101b9c6f4f29e14b92fa6b7dcafbeac3fa716b14
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-18 16:47:52 +0100

    update: added terraform remote state to get access to resource in etl state

--- core/terraform/main.tf
+++ core/terraform/main.tf
@@ -12,6 +12,16 @@ terraform {
   }
 }
 
+#to pull the output from the core etl state
+data "terraform_remote_state" "etl" {
+  backend = "s3"
+  config = {
+    bucket = "fiction-hello-etl-state-bucket"
+    key    = "terraform.tfstate" 
+    region = "eu-west-2"
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
@@ -25,14 +35,34 @@ output "instance_public_ip" {
   value = aws_instance.web.public_ip
 }
 
-output "rds_endpoint" {
+output "rds_endpoint_core" {
   value = aws_db_instance.core.endpoint
 }
+
 output "rds_endpoint_olap" {
   value = aws_db_instance.olap.endpoint
 }
 
+output "rds_sg_id" {
+  value = aws_security_group.rds_sg.id
+}
 
-output "rds_port" {
-  value = aws_db_instance.core.port
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "private1_subnet_id" {
+  value = aws_subnet.private1.id
+}
+
+output "private2_subnet_id" {
+  value = aws_subnet.private2.id
+}
+
+output "private1_subnet_cidr" {
+  value = aws_subnet.private1.cidr_block
+}
+
+output "private2_subnet_cidr" {
+  value = aws_subnet.private2.cidr_block
 }
