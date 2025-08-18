bf909aa4eb2af57bbd1f04be7015ad38c5a5b7b1
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-18 16:51:59 +0100

    update: added remote state to access core state, and added output so core state can access the resources

--- etl/terraform/provider.tf
+++ etl/terraform/provider.tf
@@ -1,3 +1,4 @@
+
 terraform {
   required_providers {
     aws = {
@@ -16,6 +17,20 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+#to pull the output from the core backend state
+data "terraform_remote_state" "backend" {
+  backend = "s3"
+  config = {
+    bucket = "fiction-hello-state-bucket"
+    key    = "terraform.tfstate" 
+    region = "eu-west-2"
+  }
+}
+
 data "aws_region" "current" {}
 
 data "aws_caller_identity" "current" {}
+
+output "lambda_sg_id" {
+  value = aws_security_group.lambda_sg.id
+}
