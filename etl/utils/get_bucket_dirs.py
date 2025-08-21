e5f1b4fc239099784b11dbc9e6b641eec29ca274
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-21 23:40:36 +0100

    feat: created the logic to get the top level directory in a bucket

---
+++ etl/utils/get_bucket_dirs.py
@@ -0,0 +1,34 @@
+import boto3
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.ERROR)
+
+def get_bucket_dirs(bucket_name: str):
+    """
+    retrieves the diretory in the provided bucket
+    name
+    -----------------------------------------
+    args: bucket_name: a sting represeting the
+    S3 bucket name
+    -----------------------------------------
+    return: list contaning all the available directories
+    """
+    try:
+        s3_client = boto3.client("s3")
+
+        s3_respones = s3_client.list_objects_v2(
+            Bucket=bucket_name,
+            Delimiter="/"
+        )
+
+        if "CommonPrefixes" not in s3_respones:
+            return []
+
+        directories = [directory["Prefix"].rstrip("/") 
+                    for directory in s3_respones["CommonPrefixes"]]
+    except Exception as e:
+        logging.error(f"retrieving directory from {bucket_name} failed: {e}")
+        raise e 
+
+    return directories
