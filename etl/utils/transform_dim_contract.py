a88886e730e3f8bd9769825f00f6560a6102b59d
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-25 21:16:28 +0100

    feat: created the logic to transofrm contracts, contract_details, periods, contract_type into dim_contract

---
+++ etl/utils/transform_dim_contract.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+
+
+def transform_dim_contract(df_contracts, df_contracts_details, df_periods, df_contract_type):
+    """
+    transform the provided tables/df into dim_contract table structure
+    -----------------------------------------
+    args: 
+    df_contracts: df contaning the contracts data.
+
+    df_contracts_details: df contaning the contracts details data.
+   
+    df_periods: df contaning the preiods data.
+
+    df_contract_type: df contaning the contract types data.
+    -----------------------------------------
+    return: datafram contaning exact structure of dim_contract table
+    """
+
+    df =  df_contracts_details
+
+    #mergning all the dfs
+    df = df.merge(df_periods, on="contract_period_id", how="left")
+    df = df.merge(df_contracts, on="contract_details_id", how="left")
+    df = df.merge(df_contract_type, on="contract_type_id", how="left")
+
+
+    #using initial and discount to get the effective price
+    df["effective_price"] = np.where(
+    df["discount_percent"] > 0,
+    df["initial_price"] * (1 - df["discount_percent"]/100),
+    df["initial_price"]
+    ).round(2)
+
+
+    #renaming the column that were merged to match the distenation table
+    df.rename(columns={"period": "contract_period"}, inplace=True)
+
+    #droppping unwanted columns
+    df = df.drop(["contract_details_id", "contract_type_id", "contract_period_id"], axis=1)
+
+    return df
