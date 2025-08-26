6b5ee6384b946590b205067c80e6767a5e49aad0
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-26 21:39:41 +0100

    feat: created the logic to transform location and customers_address into fact_customers_demographic

---
+++ etl/utils/transofrm_customers_demo.py
@@ -0,0 +1,34 @@
+
+
+
+def transform_customers_demo(df_location,df_cust_address):
+    """
+    transform the provided tables/df into 
+    tact_customers_demographic table structure
+    -----------------------------------------
+    args: 
+    df_location: df contaning the location data.
+
+    df_cust_address: df contaning the 
+    location_id and customers_id data.
+    -----------------------------------------
+    return: datafram contaning exact structure of
+    tact_customers_demographic table
+    """
+
+    df_cust_demo = df_location.copy()
+
+
+    #merging the dfs together
+    df_cust_demo = df_cust_demo.merge(df_cust_address, on="location_id", how="left")
+
+
+    #converting the date time into date to be used in the fact table
+    df_cust_demo["last_updated"] = df_cust_demo["last_updated"].dt.date
+
+    #removing unwanted columns
+    df_cust_demo.drop(["city", "county","post_code",
+                   "address_type", "full_address"],axis=1,  inplace=True)
+    
+
+    return df_cust_demo
