b9163a7a48122689770cee97e66dc708c20a7bda
Hamoud9876 <h.ghaleb95@gmail.com>
2025-08-28 21:01:06 +0100

    feat: the test for transofrm billing

---
+++ etl/test/test_transform_billing.py
@@ -0,0 +1,96 @@
+from etl.utils.transform_billing import transform_billing
+import pandas as pd
+import numpy as np
+from faker import Faker
+from datetime import timedelta, date, datetime
+import pytest
+
+
+
+fake = Faker()
+
+
+@pytest.fixture(scope="function")
+def create_data():
+    n = 5  
+
+    billing = pd.DataFrame({
+        "bill_id": range(1, n+1),
+        "customer_id": np.random.randint(1, 10, size=n),
+        "contract_id": np.random.randint(1, 10, size=n),
+        "amount": np.round(np.random.uniform(50.0, 1000.0, size=n), 2),
+        "bill_status_id": np.random.randint(1, 3, size=n),
+        "issue_date": [fake.date_time_between(start_date="-2y", end_date="now") for _ in range(n)],
+    })
+
+
+    billing["due_date"] = billing["issue_date"] + pd.to_timedelta(7, unit="d")
+
+
+    billing["completed_date"] = [
+        date + timedelta(days=np.random.randint(0, 31)) if np.random.rand() > 0.3 else None
+        for date in billing["issue_date"]]
+
+    billing["created_at"] = [fake.date_time_between(start_date="-2y", end_date="now") for _ in range(n)]
+    billing["last_updated"] = billing["created_at"] + pd.to_timedelta(np.random.randint(0, 365, size=n), unit='d')
+
+
+    billing_status = pd.DataFrame({
+        "bill_status_id": [1,2],
+        "status": ["paid", "unpaid"]
+    })
+
+    yield billing, billing_status
+
+class TestTransofromBilling:
+    def test_return_df(self, create_data):
+        df_billing, df_status = create_data
+
+        response = transform_billing(df_billing,df_status)
+
+        assert isinstance(response,pd.DataFrame)
+
+    
+    def test_contains_currect_columns(self, create_data):
+        df_billing, df_status = create_data
+
+        response = transform_billing(df_billing,df_status)
+
+        assert "bill_id" in response
+        assert "customer_id" in response
+        assert "contract_id" in response
+        assert "issue_date" in response
+        assert "due_date" in response
+        assert "completed_date" in response
+        assert "created_at" in response
+        assert "last_updated" in response
+        assert "bill_status" in response
+        assert "amount_billed" in response
+        assert "amount_paid" in response
+        assert "is_paid" in response
+        assert "days_overdue" in response
+
+
+        assert "bill_status_id" not in response
+        assert "amount" not in response
+
+
+
+    def test_output_correct_data_type(self, create_data):
+        df_billing, df_status = create_data
+
+        response = transform_billing(df_billing,df_status)
+
+        assert isinstance(response["bill_id"].loc[0],np.integer)
+        assert isinstance(response["customer_id"].loc[0],np.integer)
+        assert isinstance(response["contract_id"].loc[0],np.integer)
+        assert isinstance(response["issue_date"].loc[0], date)
+        assert isinstance(response["due_date"].loc[0],date)
+        assert isinstance(response["completed_date"].loc[0],date)
+        assert isinstance(response["created_at"].loc[0],datetime)
+        assert isinstance(response["last_updated"].loc[0],datetime)
+        assert isinstance(response["bill_status"].loc[0], str)
+        assert isinstance(response["amount_billed"].loc[0],np.floating)
+        assert isinstance(response["amount_paid"].loc[0],np.floating)
+        assert isinstance(response["is_paid"].loc[0], np.bool)
+        assert isinstance(response["days_overdue"].loc[0],np.integer)
