e975333a2b425fdaa5337594bb908078aa830187
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-04 23:13:03 +0100

    update: added logging, error handling and adjusted the logic a little bit

--- etl/utils/transform_dim_date.py
+++ etl/utils/transform_dim_date.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import numpy as np
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 def transform_dim_date(**kwargs):
@@ -16,18 +20,45 @@ def transform_dim_date(**kwargs):
 
     all_dates = []
 
-    for df_name, df in kwargs.items():
-        for col in df.columns:
-            if np.issubdtype(df[col].dtype, np.datetime64):
-                all_dates.append(df[col].dropna())
+    logger.info("started transforming dim_date")
 
-    all_dates = pd.concat(all_dates).drop_duplicates().reset_index(drop=True)
-    dim_date = pd.DataFrame({"date_id": all_dates,
-                            "day": all_dates.dt.day,
-                            "month": all_dates.dt.month,
-                            "year": all_dates.dt.year,
-                            "day_of_week": all_dates.dt.dayofweek,
-                            "day_name": all_dates.dt.day_name(),
-                            "quarter": all_dates.dt.quarter})
+    try:
+        for df_name, df in kwargs.items():
+            for col in df.columns:
+                if df[col].dtype == 'object':
+                    try:
+                        df[col] = pd.to_datetime(df[col], errors='coerce')
+                    except Exception:
+                        continue
+            
+                if np.issubdtype(df[col].dtype, np.datetime64):
+                    all_dates.append(df[col].dropna())
+
+        if all_dates:
+            all_dates = pd.concat(all_dates).drop_duplicates().reset_index(drop=True)
+            dim_date = pd.DataFrame({"date_id": all_dates,
+                                    "day": all_dates.dt.day,
+                                    "month": all_dates.dt.month,
+                                    "year": all_dates.dt.year,
+                                    "day_of_week": all_dates.dt.dayofweek,
+                                    "day_name": all_dates.dt.day_name(),
+                                    "quarter": all_dates.dt.quarter})
+        else:
+            logger.info("no dates columns found")
+            return pd.DataFrame(columns=[
+                "date_id", 
+                "day", 
+                "month", 
+                "year", 
+                "day_of_week", 
+                "day_name", 
+                "quarter"
+            ])
+
+    except Exception as e:
+        logger.error(f"failed to transform dim_date: {e}")
+
+
+    logger.info("finished transforming dim_date")
     
     return dim_date
