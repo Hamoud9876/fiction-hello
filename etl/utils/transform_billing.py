050c4dcd2f7e0d34e25038b874010606a662f9f9
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-04 23:10:37 +0100

    update: added loggin and error handling

--- etl/utils/transform_billing.py
+++ etl/utils/transform_billing.py
@@ -1,4 +1,9 @@
 from datetime import date
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
 
 def transform_billing(df_billing, df_billing_status):
     """
@@ -13,44 +18,50 @@ def transform_billing(df_billing, df_billing_status):
     fact_billing table.
     """
 
-    #making sure dates are in the correct datatype
-    df_billing["completed_date"] = df_billing["completed_date"].dt.date
-    df_billing["issue_date"] = df_billing["issue_date"].dt.date
-    df_billing["due_date"] = df_billing["due_date"].dt.date
+    logger.info("started transforming billing")
 
-    #filling empty completed date with a base date
-    df_billing["completed_date"] = df_billing["completed_date"].fillna(
-        date(year=1995,day=1,month=1))
+    try:
+        #making sure dates are in the correct datatype
+        df_billing["completed_date"] = df_billing["completed_date"].dt.date
+        df_billing["issue_date"] = df_billing["issue_date"].dt.date
+        df_billing["due_date"] = df_billing["due_date"].dt.date
 
-    #getting the status descreption
-    df_billing = df_billing.merge(df_billing_status, on="bill_status_id", how="left")
+        #filling empty completed date with a base date
+        df_billing["completed_date"] = df_billing["completed_date"].fillna(
+            date(year=1995,day=1,month=1))
 
-    #getting the billed and paid amount
-    df_billing["amount_billed"] = df_billing["amount"]
-    df_billing["amount_paid"] = df_billing.apply(
-        lambda row: row["amount"] 
-        if row["status"] == "paid" else 0,
-        axis=1)
+        #getting the status descreption
+        df_billing = df_billing.merge(df_billing_status, on="bill_status_id", how="left")
 
-    #setting the is_paid value
-    df_billing["is_paid"] = df_billing.apply(
-        lambda row: True 
-        if row["status"]== "paid" else False,
-        axis=1)
+        #getting the billed and paid amount
+        df_billing["amount_billed"] = df_billing["amount"]
+        df_billing["amount_paid"] = df_billing.apply(
+            lambda row: row["amount"] 
+            if row["status"] == "paid" else 0,
+            axis=1)
 
-    #calculating overdue days
-    df_billing["days_overdue"] = df_billing.apply(
-        lambda row: 0 
-        if row["due_date"] >= row["completed_date"] 
-        else (row["completed_date"] -row["due_date"] ).days,
-        axis=1 )
+        #setting the is_paid value
+        df_billing["is_paid"] = df_billing.apply(
+            lambda row: True 
+            if row["status"]== "paid" else False,
+            axis=1)
+
+        #calculating overdue days
+        df_billing["days_overdue"] = df_billing.apply(
+            lambda row: 0 
+            if row["due_date"] >= row["completed_date"] 
+            else (row["completed_date"] -row["due_date"] ).days,
+            axis=1 )
 
 
-    #dropping unwated columns
-    df_billing.drop(["bill_status_id","amount"],axis=1,inplace=True)
+        #dropping unwated columns
+        df_billing.drop(["bill_status_id","amount"],axis=1,inplace=True)
 
-    #renaming columns
-    df_billing.rename(columns={"status": "bill_status"}, inplace=True)
+        #renaming columns
+        df_billing.rename(columns={"status": "bill_status"}, inplace=True)
+    except Exception as e:
+        logger.error(f"failed to transform billing")
 
+    logger.info("finished transforming billing")
 
     return df_billing
