1a9e83d923a508531657f8bdd4ced7de4ec01da5
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-07 16:09:45 +0100

    update: removed unnecessary parameter and updated the code accourdingly

--- etl/utils/transform_customers_demo.py
+++ etl/utils/transform_customers_demo.py
@@ -4,13 +4,12 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def transform_customers_demo(df_location,df_cust_address):
+def transform_customers_demo(df_cust_address):
     """
     transform the provided tables/df into 
     tact_customers_demographic table structure
     -----------------------------------------
     args: 
-    df_location: df contaning the location data.
 
     df_cust_address: df contaning the 
     location_id and customers_id data.
@@ -21,20 +20,15 @@ def transform_customers_demo(df_location,df_cust_address):
     
     logger.info("started transforming customers_demo")
 
-    df_cust_demo = df_location.copy()
-    df_copy_cust_address = df_cust_address.copy
-
-    #merging the dfs together
-    df_cust_demo = df_cust_demo.merge(df_copy_cust_address, on="location_id", how="left")
-
-
-    #converting the date time into date to be used in the fact table
-    df_cust_demo["last_updated"] = df_cust_demo["last_updated"].dt.date
-
-    #removing unwanted columns
-    df_cust_demo.drop(["city", "county","post_code",
-                   "address_type", "full_address"],axis=1,  inplace=True)
+    try:
+        df_copy_cust_address = df_cust_address.copy()
     
+
+        #removing unwanted columns
+        df_copy_cust_address.drop(["customers_address_id"],axis=1, inplace=True)
+    
+    except Exception as e:
+        logger.error(f"failed to transform customers_demo: {type(e).__name__}: {e}")
     logger.info("finished transforming customers_demo")
 
-    return df_cust_demo
+    return df_copy_cust_address
