ce5aa3963e8cd0fb7c353d6d993dc57858953c9f
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-08 19:44:10 +0100

    feat: created the lambda handler and the requirements file for lambda load

---
+++ etl/src/lambda_load/lambda_load.py
@@ -0,0 +1,18 @@
+
+
+
+def lambda_load(event, context):
+    """
+    Loads files in processed bucjet to their destenation
+    database.
+
+
+    args: 
+        event: AWS required (not used)
+        context: AWS required (not used)
+
+    
+    return: 
+        dict: Status code 200 if loaded with no problems
+    """
+    pass
