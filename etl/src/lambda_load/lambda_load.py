f6821acd744a0673ff74aae5f35e3e6c79ead805
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-12 17:06:55 +0100

    update: added the full logic to load data into db

--- etl/src/lambda_load/lambda_load.py
+++ etl/src/lambda_load/lambda_load.py
@@ -1,3 +1,11 @@
+from etl.utils.get_bucket_dirs import get_bucket_dirs
+from etl.utils.read_parquet_file import read_parquet_file
+from etl.utils.load_into_olap import insert_df_into_db
+from datetime import datetime
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
 
@@ -7,12 +15,31 @@ def lambda_load(event, context):
     database.
 
 
-    args: 
+    Args: 
         event: AWS required (not used)
         context: AWS required (not used)
 
-    
     return: 
         dict: Status code 200 if loaded with no problems
     """
-    pass
+
+    bucket_name = "etl-process-bucket-2025"
+
+    directories = get_bucket_dirs(bucket_name)
+    timestamp = datetime.now()
+    
+    # Step 1: Read parquet
+    for drictory in directories:
+        df = read_parquet_file(bucket_name, timestamp,drictory)
+    
+        
+        insert_df_into_db(df, drictory)
+
+        logger.info(f"rows_inserted: {len(df)} in {drictory}")
+
+    return {"status": 200}
+
+
+
+
+
