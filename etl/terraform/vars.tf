eca43cce17fc55ce2428292d08caa432fcc56d2d
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-12 21:46:37 +0100

    update: added new vars for olap db

--- etl/terraform/vars.tf
+++ etl/terraform/vars.tf
@@ -1,3 +1,32 @@
+variable "DB_OLTP_USERNAME" {
+  type = string
+  default = "User"
+}
+
+
+variable "DB_OLTP_PASSWORD" {
+  type = string
+  default = "password"
+}
+
+
+variable "DB_OLTP_DATABASE" {
+  type = string
+  default = "db_name"
+}
+
+
+variable "DB_OLTP_HOST" {
+  type = string
+  default = "host"
+}
+
+
+variable "DB_OLTP_PORT" {
+  type = string
+  default = "port"
+}
+
 variable "DB_OLAP_USERNAME" {
   type = string
   default = "User"
