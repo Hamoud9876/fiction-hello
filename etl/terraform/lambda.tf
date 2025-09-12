8cc1b56870ce85f11299162dd792bfa09f2ad25f
Hamoud9876 <h.ghaleb95@gmail.com>
2025-09-12 21:45:34 +0100

    update: added new lambda load

--- etl/terraform/lambda.tf
+++ etl/terraform/lambda.tf
@@ -6,10 +6,10 @@ data "archive_file" "ingestion_lambda" {
 }
 
 
-#this implementation of the lambda
+#lambda ingestion (extract)
 resource "aws_lambda_function" "ingestion_lambda" {
   filename         = "${path.module}/../archives/lambda_ingestion/lambda_ingestion.zip"
-  function_name    = "ingestion"
+  function_name    = "lambda_ingestion"
   role             = aws_iam_role.lambda_role.arn
   handler          = "lambda_ingestion.ingest_data"
   runtime          = "python3.10"
@@ -22,11 +22,11 @@ resource "aws_lambda_function" "ingestion_lambda" {
 
   environment {
     variables = {
-      DB_OLAP_USERNAME= var.DB_OLAP_USERNAME
-      DB_OLAP_PASSWORD= var.DB_OLAP_PASSWORD
-      DB_OLAP_DATABASE= var.DB_OLAP_DATABASE
-      DB_OLAP_HOST= var.DB_OLAP_HOST
-      DB_OLAP_PORT= var.DB_OLAP_PORT
+      DB_OLAP_USERNAME= var.DB_OLTP_USERNAME
+      DB_OLAP_PASSWORD= var.DB_OLTP_PASSWORD
+      DB_OLAP_DATABASE= var.DB_OLTP_DATABASE
+      DB_OLAP_HOST= var.DB_OLTP_HOST
+      DB_OLAP_PORT= var.DB_OLTP_PORT
     }
   }
 
@@ -45,7 +45,7 @@ resource "aws_lambda_function" "ingestion_lambda" {
   depends_on = [ data.archive_file.ingestion_lambda ]
 }
 
-
+#transform lambda
 resource "aws_lambda_function" "lambda_transform" {
   function_name = "lambda-transform"
   package_type  = "Image"
@@ -54,3 +54,43 @@ resource "aws_lambda_function" "lambda_transform" {
   timeout          = 521
   memory_size      = 1024
 }
+
+#archiving the function itself to be shipped
+data "archive_file" "load_lambda" {
+  type        = "zip"
+  source_file = "${path.module}/../src/lambda_load/lambda_load.py"
+  output_path = "${path.module}/../archives/lambda_load/lambda_load.zip"
+}
+
+#load lambda
+resource "aws_lambda_function" "load_lambda" {
+  filename         = "${path.module}/../archives/lambda_load/lambda_load.zip"
+  function_name    = "lambda_load"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_load.lambda_load"
+  runtime          = "python3.10"
+  timeout          = 521
+  memory_size      = 1024
+
+
+  layers =  [aws_lambda_layer_version.load_layer.arn,
+  "arn:aws:lambda:eu-west-2:336392948345:layer:AWSSDKPandas-Python310:25" ]
+ 
+
+  environment {
+    variables = {
+      DB_OLAP_USERNAME= var.DB_OLAP_USERNAME
+      DB_OLAP_PASSWORD= var.DB_OLAP_PASSWORD
+      DB_OLAP_DATABASE= var.DB_OLAP_DATABASE
+      DB_OLAP_HOST= var.DB_OLAP_HOST
+      DB_OLAP_PORT= var.DB_OLAP_PORT
+    }
+  }
+  tags = {
+    Environment = "production"
+    Application = "example"
+  }
+  depends_on = [ data.archive_file.load_lambda]
+}
+
+
